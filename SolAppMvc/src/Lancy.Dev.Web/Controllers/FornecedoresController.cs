74333291737a56892aa1c1296aa3c1ce1502dd9f
Marcos Lancy <marcos.system32@gmail.com>
2020-04-05 14:51:25 -0300

    Adição modal edição endereço com ajax

--- SolAppMvc/src/Lancy.Dev.Web/Controllers/FornecedoresController.cs
+++ SolAppMvc/src/Lancy.Dev.Web/Controllers/FornecedoresController.cs
@@ -12,13 +12,16 @@ namespace Lancy.Dev.Web.Controllers
     public class FornecedoresController : BaseController
     {
         private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IEnderecoRepository _enderecoRepository;
         private readonly IMapper _mapper;
 
         public FornecedoresController(IFornecedorRepository fornecedorRepository,
+                                      IEnderecoRepository enderecoRepository,
                                       IMapper mapper)
         {
             _mapper = mapper;
             _fornecedorRepository = fornecedorRepository;
+            _enderecoRepository = enderecoRepository;
         }
 
 
@@ -104,6 +107,43 @@ namespace Lancy.Dev.Web.Controllers
             await _fornecedorRepository.Remover(id);
 
             return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> ObterEndereco(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEndereco(id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+            return PartialView("_DetalhesEndereco", fornecedor);
+        }
+
+        //chama e popula PartialView
+        public async Task<IActionResult> AtualizarEndereco(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEndereco(id);
+
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView("_AtualizarEndereco", new FornecedorViewModel { Endereco = fornecedor.Endereco });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AtualizarEndereco(Guid id, FornecedorViewModel fornecedorViewModel)
+        {
+            ModelState.Remove("Nome");
+            ModelState.Remove("Documento");
+            if(!ModelState.IsValid) return PartialView("_AtualizarEndereco", fornecedorViewModel);
+
+            await _enderecoRepository.Atualizar(_mapper.Map<Endereco>(fornecedorViewModel.Endereco));
+
+            var url = Url.Action("ObterEndereco", "Fornecedores", new { id = fornecedorViewModel.Endereco.FornecedorId });
+            return Json(new { success = true, url });
         }
 
         private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id)
