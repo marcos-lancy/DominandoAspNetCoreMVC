3194f064f57d642e4a61880d0ac5291ae392a4c2
Marcos Lancy <marcos.system32@gmail.com>
2020-04-06 13:58:59 -0300

    Adiciona página de erro, custon páigna de erro

--- SolAppMvc/src/Lancy.Dev.Web/Startup.cs
+++ SolAppMvc/src/Lancy.Dev.Web/Startup.cs
@@ -55,7 +55,8 @@ namespace Lancy.Dev.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/500");
+                app.UseStatusCodePagesWithRedirects("/erro/{0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
